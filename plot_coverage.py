90d878fbc64a29f8f5547043dbb68db9ca924750
Joao Ferreira <jf18@sanger.ac.uk>
2022-04-26 18:10:19 +0100

    Add option to subset target sequences

--- plot_coverage.py
+++ plot_coverage.py
@@ -10,7 +10,7 @@ from get_depth import get_depth, get_windows_depth
 from covered_seqs import get_covered_seqs
 from make_genome import make_genome_file, make_genome_windows
 from seq_parser import get_all_seqs
-from sort_sequences import sort_sequences
+from sort_sequences import sort_sequences, retrieve_sequences
 from merge_images import merge_images
 
 def plot_coverage(depth_file, out_filename, seq_ID, winSize):
@@ -30,28 +30,28 @@ def plot_coverage(depth_file, out_filename, seq_ID, winSize):
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--bam', help='BAM mapping file')
-    parser.add_argument('--winSize', help='Windows size')
-    parser.add_argument('--maxNumSeqs', help='Maximum number of sequences')
-    parser.add_argument('--img', help='List of images to concatenate')
-    parser.add_argument('--depth', help='Depth file to use as input for generating plot')
+    parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+    mutually_exclusive_group = optional.add_mutually_exclusive_group(required=False)
+    mutually_exclusive_group.add_argument('--maxNumSeqs', help='Maximum number of sequences')
+    mutually_exclusive_group.add_argument('--seqs', help='List of sequences to be used')
+    required.add_argument('--bam', help='BAM mapping file')
+    optional.add_argument('--winSize', help='Windows size')
+    optional.add_argument('--img', help='List of images to concatenate')
+    optional.add_argument('--depth', help='Depth file to use as input for generating plot')
     args = parser.parse_args()
     
     if args.bam:
         in_bam = args.bam
-        
-        #print('Splitting BAM file according to reference sequence...')
-        #split_bam(in_bam)
-        
-        # get list of covered sequences and save to `covered_seqs.txt` file
-        #print("Retrieving list of covered sequences...")
-        #covered_seqs = get_covered_seqs(in_bam, "covered_seqs.txt")
         
         start = time.time()
         print("Getting length information from genome sequences", flush=True)
         seqs_info = get_all_seqs(in_bam)
         if args.maxNumSeqs:
             seqs_info_sliced = sort_sequences(seqs_info, args.maxNumSeqs)
+        elif args.seqs:
+            seqs_info_sliced = retrieve_sequences(seqs_info, args.seqs)
         else:
             seqs_info_sliced = sort_sequences(seqs_info, len(seqs_info))
         end = time.time()
@@ -63,42 +63,42 @@ def main():
           seq_ID, seq_len = row['seq_ID'], str(row['seq_len'])
           # create genome file
           start = time.time()
-          print(f"Creating genome file for {seq_ID}")
+          print(f"Creating genome file for {seq_ID}", flush=True)
           genome_filename = f"{seq_ID}.genome.txt"
           with open(genome_filename, "w") as f:
             f.write("\t".join([seq_ID, seq_len]))
           end = time.time()
           elapsed_time = end - start
-          print(f"Elapsed time: {elapsed_time}")
+          print(f"Elapsed time: {elapsed_time}", flush=True)
           
           # create windows file
           start = time.time()
-          print(f"Creating genome windows file for {seq_ID}")
+          print(f"Creating genome windows file for {seq_ID}", flush=True)
           windows_filename = make_genome_windows(genome_filename, args.winSize)
           end = time.time()
           elapsed_time = end - start
-          print(f"Elapsed time: {elapsed_time}")
+          print(f"Elapsed time: {elapsed_time}", flush=True)
           
           # calculate mean depth per windows
           start = time.time()
-          print(f"Calculating mean depth per windows for {seq_ID}")
+          print(f"Calculating mean depth per windows for {seq_ID}", flush=True)
           depth_per_windows = get_windows_depth(windows_filename, in_bam, seq_ID)
           end = time.time()
           elapsed_time = end - start
-          print(f"Elapsed time: {elapsed_time}")
+          print(f"Elapsed time: {elapsed_time}", flush=True)
           
           # creating plot for sequence
           start = time.time()
-          print(f"Creating coverage plot for {seq_ID}")
+          print(f"Creating coverage plot for {seq_ID}", flush=True)
           out_plot_filename = f"{seq_ID}.coverage.png"
           seqs_plots.append(out_plot_filename)
           plot_coverage(depth_per_windows, out_plot_filename, seq_ID, int(args.winSize))
           end = time.time()
           elapsed_time = end - start
-          print(f"Elapsed time: {elapsed_time}")
+          print(f"Elapsed time: {elapsed_time}", flush=True)
         
-        print(f"seqs_plots: {seqs_plots}")
-        print("Concatenating sequences coverages plots into final.coverage.png")
+        print(f"seqs_plots: {seqs_plots}", flush=True)
+        print("Concatenating sequences coverages plots into final.coverage.png", flush=True)
         merge_images(seqs_plots, "final.coverage.png")
     
     if args.depth:
@@ -108,7 +108,7 @@ def main():
 
     if args.img:
         images = args.img.split()
-        print("Concatenating images into final.coverage.png")
+        print("Concatenating images into final.coverage.png", flush=True)
         merge_images(images, "final.coverage.png")
    
 if __name__ == "__main__":
--- sort_sequences.py
+++ sort_sequences.py
@@ -9,5 +9,16 @@ def sort_sequences(in_df, upp_lim):
 
     return df1_sorted_sliced
 
+def retrieve_sequences(in_df, target_seqs_file):
+    target_seqs_IDs = set()
+    with open(target_seqs_file, "r") as f:
+        for line in f:
+            target_seq_ID = line.strip()
+            target_seqs_IDs.add(target_seq_ID)
+
+    df1 = in_df.loc[in_df['seq_ID'].isin(target_seqs_IDs)]
+    
+    return df1
+
 if __name__ == "__main__":
     sort_sequences(sys.argv[1], sys.argv[2])
